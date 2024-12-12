b68f28bdd9c990eeee899f768b6db21a08478a58
Albert Panesse <albert.panesse@gmail.com>
2024-12-12 09:33:51 +0700

    ok #1

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 export const ENV = {
-  "API_URL": "http://172.18.100.20:3000"
+  "API_URL": "http://10.147.17.139:3000"
 };
--- src/app/libs/services/auth.service.ts
+++ src/app/libs/services/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
         services: {
           apiService: this.apiService,
           commonService: this.commonService,  
-          authService: this,
+          store: this.store,
         },
       },
     }).start();
